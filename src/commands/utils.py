f9aba56723b4ad9939335dd1162b7bab5c5e3755
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-04-05 15:31:11 -0500

    Add help command

--- src/commands/utils.py
+++ src/commands/utils.py
@@ -2,7 +2,23 @@ import click
 
 import click_utils
 
-__all__ = ['echo']
+
+__all__ = ['echo', 'help']
+
+
+@click.command()
+@click_utils.help_option()
+@click.pass_context
+def help(ctx):
+	""" Help text for the applicaiton. Type 'help' to view this message.
+
+		Available Commands: 
+		clear, echo, help	
+
+		Use '[command] --help' for more information on a specific command. 
+	"""
+	click_utils.print_help(ctx, None, True)
+
 
 @click.command()
 @click_utils.help_option()
