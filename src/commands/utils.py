ac1971321302cc45a5b4549486f3c51ea3fc0909
elprice <elprice11@gmail.com>
2018-04-09 02:09:20 -0700

    Added Directory model. Created demo table with directories. Updated User model to keep up with it's working directory. Added 'working', 'cd', and 'ls' commands with basic functionality.

--- src/commands/utils.py
+++ src/commands/utils.py
@@ -3,10 +3,10 @@ from flask_login import current_user, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 import click_utils
-from models import User
+from models import User, Directory
 
 
-__all__ = ['echo', 'help', 'login', 'logout', 'passwd']
+__all__ = ['echo', 'help', 'login', 'logout', 'passwd', 'working', 'cd', 'ls']
 
 
 @click.command()
@@ -67,3 +67,61 @@ def passwd(new_pass):
 	current_user.password_hash = generate_password_hash(new_pass)
 	current_user.save()
 	return "Password changed."
+
+@click.command()
+@click_utils.help_option()
+@click.argument('directory_name')
+def cd(directory_name):
+	""" Change directory to the provided directory_name. """
+	if not current_user.is_authenticated:
+		return "Must be logged in to change directories."
+
+	#look up
+	if directory_name == "..":
+		if current_user.working_directory != 0:
+			return move_working_up()
+		else:
+			return "Working directory is already root."
+
+	#look down
+	if not Directory.select().where((Directory.name == directory_name) & (Directory.parent == current_user.working_directory)).exists():
+		return "Directory " + Directory.get(Directory.id == current_user.working_directory).get_full_path() + "\\" + directory_name + " not found."
+	else:	
+		return move_working_down(directory_name)
+
+@click.command()
+@click_utils.help_option()
+def working():
+	""" Returns the current working directory path. """
+	if not current_user.is_authenticated:
+		return "Must be logged in to check current working directory path."
+
+	return Directory.get(Directory.id == current_user.working_directory).get_full_path()
+
+@click.command()
+@click_utils.help_option()
+def ls():
+	""" Returns the current working directory path. """
+	if not current_user.is_authenticated:
+		return "Must be logged in to check current working directory path."
+
+	children = Directory.select().where(Directory.parent == current_user.working_directory)
+	if not children:
+		return "No child directories found."
+	else:
+		return "</br>".join([child.get_full_path() for child in children])
+
+	return Directory.get(Directory.id == current_user.working_directory).get_full_path()
+
+
+def move_working_up():
+	parent_id = Directory.get(Directory.id == current_user.working_directory).parent
+	current_user.working_directory = parent_id
+	current_user.save()
+	return "Working directory is now " + Directory.get(Directory.id == parent_id).get_full_path()
+
+def move_working_down(directory_name):
+	child = Directory.get((Directory.name == directory_name) & (Directory.parent == current_user.working_directory))
+	current_user.working_directory = child.id
+	current_user.save()
+	return "Working directory is now " + child.get_full_path()
--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ from werkzeug.security import generate_password_hash
 
 from helpers import env
 
+import logging
+
 
 db = PostgresqlDatabase('gonano', user=env.data_db_user, password=env.data_db_pass, host=env.data_db_host)
 
@@ -16,6 +18,7 @@ class BaseModel(Model):
 class User(BaseModel, UserMixin):
 	username = CharField(unique=True)
 	password_hash = CharField()
+	working_directory = IntegerField()
 
 	def exists(username=None, id=None):
 		if username is None and id is None:
@@ -28,9 +31,39 @@ class User(BaseModel, UserMixin):
 			return User.select().where(User.username == username).exists()
 
 
-models = [User]
+class Directory(BaseModel):
+	id = IntegerField()
+	parent = IntegerField()
+	depth = IntegerField()
+	name = CharField()
+
+	def get_full_path(self):
+		if self.id is not None:
+			if self.id is not 0:
+				return Directory.get(Directory.id == self.parent).get_full_path() + '\\' + self.name
+			else:
+				return self.name
+
+#maybe what a File will look like
+class File(BaseModel):
+	id = IntegerField()
+	directory_id = IntegerField()
+	name = CharField()
+	content = BlobField()
+	extension = CharField()
+
+models = [User, Directory, File]
+
 for model in models: 
+	model.drop_table(fail_silently=True)
 	model.create_table(fail_silently=True)
 
 if not User.select().where(User.username == 'root').exists():
-	User.create(username='root', password_hash=generate_password_hash('toor'))
+	User.create(username='root', password_hash=generate_password_hash('toor'), working_directory=1)
+
+#demo data for testing
+if not Directory.select().where(Directory.id == 0 or Directory.id == 1 or Directory.id == 2 or Directory.id == 3).exists():
+	Directory.create(id=0, parent=-1, depth=0, name='root')
+	Directory.create(id=1, parent=0, depth=1, name='first')
+	Directory.create(id=2, parent=1, depth=2, name='second')
+	Directory.create(id=3, parent=0, depth=1, name='first2')
