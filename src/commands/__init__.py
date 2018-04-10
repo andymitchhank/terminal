240761966c5ffdb7d762f76463f3a30b9add93bb
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-04-10 10:02:42 -0500

    Simplify commands init

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,8 +1,9 @@
-from . import utils
-from . import group_example
-
-__all__ = utils.__all__ + group_example.__all__
-
 from .utils import *
-from .group_example import * 
+from .group_example import *
+
+__all__ = []
+
+for key, value in list(locals().items()):
+    if callable(value):
+        __all__.append(key)
 
