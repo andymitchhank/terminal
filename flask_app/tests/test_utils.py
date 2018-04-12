89d20494b9ca7009528d4f95aa4752c69553e722
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-04-12 11:39:32 -0500

    Fix no ctx and add a few more echo tests.

--- flask_app/tests/test_utils.py
+++ flask_app/tests/test_utils.py
@@ -5,6 +5,23 @@ from commands.utils import echo
 
 def test_echo_returns_passed_in():
 	to_echo = 'testing'
-	result = echo.callback(['testing'], False)
+	result = echo.main(args=[to_echo], standalone_mode=False)
 	assert to_echo == result
+
+
+def test_echo_returns_stdout_with_no_params():
+	to_echo = 'testing'
+	obj = {'stdout': to_echo}
+	result = echo.main(args=[], standalone_mode=False, obj=obj)
+	assert to_echo == result
+
+
+def test_echo_ignores_stdout_with_params():
+	to_echo = 'testing'
+	stdout = 'stdout'
+	obj = {'stdout': stdout}
+	result = echo.main(args=[to_echo], standalone_mode=False, obj=obj)
+	assert to_echo != stdout
+	assert to_echo == result
+
 
