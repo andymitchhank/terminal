924744046cd88aed2cc4c635cbf6250d76780598
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-04-23 15:31:26 -0500

    Add result interfaces

--- react_app/src/api_services.tsx
+++ react_app/src/api_services.tsx
@@ -6,11 +6,21 @@ let runCommand = async (command: string) => {
       body: JSON.stringify({ command: command })
     });
   return await res.json();
-}
+};
 
 let getPrompt = async () => {
   const res = await fetch('/prompt', { credentials: 'same-origin' });
   return await res.text();
+};
+
+interface TerminalResult {
+  prompt: string;
+  stdout: string;
 }
 
-export { runCommand, getPrompt };
+interface EditorResult {
+  path: string;
+  content: string;
+}
+
+export { runCommand, getPrompt, TerminalResult, EditorResult };
